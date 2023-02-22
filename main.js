b860b0ec66a155ab858ac4526b2a2f4809a77f4a
Pratik Ramteke <pratikramteke222@gmail.com>
2023-02-22 19:22:35 +0530

    api added

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const input = document.querySelector("input");
+const apiKey = process.env.API_KEY;
 
 input.addEventListener("keyup", (e) => {
   if (e.key === "Enter" && input.value != "") {
     console.log(input.value);
+    getData(input.value);
     input.value = "";
   }
 });
+
+function getData(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => alert(data.main.temp));
+}
