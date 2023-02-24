90575c22172c12a9ff45a24a14582bbf499b1e07
Pratik Ramteke <pratikramteke222@gmail.com>
2023-02-24 11:32:25 +0530

    weather app added

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-const input = document.querySelector("input");
-const apiKey = process.env.API_KEY;
+// Data => Done
+// Variable to Store the Element => Done
+// Function to get the data from weather app
+// Manipluate the varibe of already created element
 
-input.addEventListener("keyup", (e) => {
-  if (e.key === "Enter" && input.value != "") {
-    console.log(input.value);
-    getData(input.value);
-    input.value = "";
+let data;
+
+const time = document.getElementById("time");
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const feelsLike = document.getElementById("feelsLike");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temprature = document.getElementById("temprature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  if (!inputBox.value) {
+    alert("Please Enter The City Name: ");
+    return;
   }
-});
 
-function getData(city) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-  )
-    .then((res) => res.json())
-    .then((data) => alert(data.main.temp));
-}
+  //
+  const city = inputBox.value;
+
+  // Fetch Details
+
+  const fetchData = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`
+  );
+  console.log(fetchData);
+  const orgData = await fetchData.json();
+  data = orgData;
+  console.log(data);
+
+  // Displaying the data in HTML
+  time.innerHTML = data.location.localtime;
+  countryName.innerHTML = data.location.country;
+  stateName.innerHTML = data.location.region;
+  cityName.innerHTML = data.location.name;
+  feelsLike.innerHTML = data.current.feelslike_c;
+  humidity.innerHTML = data.current.humidity;
+  windSpeed.innerHTML = data.current.wind_kph;
+  temprature.innerHTML = data.current.temp_c;
+  logoImage.src = data.current.condition.icon;
+  weatherStatus.innerHTML = data.current.condition.text;
+});
